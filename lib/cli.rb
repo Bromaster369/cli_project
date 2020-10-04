6423acff52cfdde08a2a23aeb23dbe99728771b8
Bromaster369 <chrispardopersonal@gmail.com>
2020-10-04 15:13:53 -0500

    message

---
+++ lib/cli.rb
@@ -0,0 +1,4 @@
+#interactions with user
+#contain all of our gets and puts 
+# control the flow of our program 
+
---
+++ lib/make.rb
@@ -0,0 +1,4 @@
+# keep track of our makes 
+# turn our responses into objects 
+# save all makes created 
+
